8d48b65d18b2c010b6b28699016fda6d79a3bcbe
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-18 20:04:22 -0400

    displays new ingredients

--- app/page.tsx
+++ app/page.tsx
@@ -9,38 +9,33 @@ interface Ingredient {
   quantity: number;
   unit: string;
   name: string;
-  added: boolean;
 }
 
-const initialItems = [
+const testItems = [
   {
     id: 1,
     quantity: 40,
     unit: "ml",
     name: "Viscous Lidocaine 2%",
-    added: false,
   },
-  { id: 2, quantity: 40, unit: "ml", name: "Maalox", added: false },
-  { id: 3, quantity: 40, unit: "ml", name: "Diphenhydramine", added: false },
+  { id: 2, quantity: 40, unit: "ml", name: "Maalox" },
+  { id: 3, quantity: 40, unit: "ml", name: "Diphenhydramine" },
 ];
 
 export default function Home() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
   return (
     <div>
       <Navbar />
-      <IngredientList ingredients={initialItems} />
-      {/* <main className="dark:bg-gray-00 flex min-h-screen items-center justify-center gap-2">
-        <h1 className="text-2xl dark:text-white">Compounding Recipes</h1>
-        <DarkThemeToggle />
-      </main> */}
+      <IngredientList ingredients={items} setItems={setItems} />
     </div>
   );
 }
 
 interface IngredientListProps {
   ingredients: Array<Ingredient>;
+  setItems: Function;
 }
 
 function IngredientList(props: IngredientListProps) {
@@ -48,11 +43,29 @@ function IngredientList(props: IngredientListProps) {
   const [unit, setUnit] = useState("");
   const [name, setName] = useState("");
 
-  function handleAdd(event) {
+  function handleAdd(event: any) {
     event.preventDefault();
 
     const newIngredient = { quantity, unit, name, id: Date.now() };
     console.log(newIngredient);
+
+    // props.setItems((prev: Array<Ingredient>) => {
+    //   let x = [
+    //     // with a new array
+    //     ...prev, // that contains all the old items
+    //     newIngredient,
+    //   ];
+
+    //   console.log(x);
+    // });
+
+    let old = props.ingredients.slice();
+    old.push(newIngredient);
+    props.setItems(old);
+
+    // setItems((items) => [...items, item]);
+
+    // props.setItems((newIngredient: any) => [...newIngredient]);
   }
 
   function handleDelete() {
@@ -60,7 +73,7 @@ function IngredientList(props: IngredientListProps) {
   }
 
   return (
-    <div className="container max-w-lg rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
+    <div className="container max-w-3xl rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
       <h1 className="mb-3 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
         Title : Magic Mouthwash
       </h1>
@@ -68,35 +81,114 @@ function IngredientList(props: IngredientListProps) {
         <h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Ingredients
         </h5>
-        <input type="text" />
-        <button
-          type="button"
-          onClick={handleAdd}
-          className="mb-2 me-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-        >
-          {" "}
-          ➕ Add
-        </button>
-        <button
-          type="button"
-          onClick={handleDelete}
-          className="mb-2 me-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-        >
-          ❌ Delete
-        </button>
-        <ul>
-          {props.ingredients.map((item: Ingredient) => (
-            <li key={item.id}>
-              {" "}
-              {item.quantity} {item.unit} {item.name}
-            </li>
-          ))}
-          <li></li>
-        </ul>
+        <input
+          type="text"
+          value={quantity}
+          onChange={(event) => setQuantity(Number(event.target.value))}
+        />
+        <span>
+          <button
+            type="button"
+            onClick={handleAdd}
+            className="mb-2 me-2 rounded-full border border-gray-200 bg-white p-4 px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+          >
+            {" "}
+            ➕ Add
+          </button>
+          <button
+            type="button"
+            onClick={handleDelete}
+            className="me-2rounded-full mb-2 border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+          >
+            ❌ Delete
+          </button>
+        </span>
+        <div className="list">
+          <ul>
+            {/* {props.ingredients.map((item: Ingredient) => (
+              <li key={item.id}>
+                {" "}
+                {item.quantity} {item.unit} {item.name} */}
 
-        <p></p>
-      </div>
-      {/* <h5 className="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {props.ingredients.map((item: Ingredient) => (
+              <li key={item.id}>
+                {" "}
+                {item.quantity} {item.unit} {item.name}
+              </li>
+            ))}
+            <li></li>
+          </ul>
+
+          <form>
+            <div className="mb-6 grid gap-6 md:grid-cols-5">
+              <div>
+                <label
+                  htmlFor="quantity_input"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Quantity
+                </label>
+                <input
+                  type="number"
+                  id="quantity_input"
+                  value={quantity}
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  required
+                  onChange={(event) => setQuantity(Number(event.target.value))}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="unit_input"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Unit
+                </label>
+                <input
+                  type="text"
+                  id="unit_input"
+                  value={unit}
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  required
+                  onChange={(event) => setUnit(event.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="name_input"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Medication Name
+                </label>
+                <input
+                  type="text"
+                  id="name_input"
+                  value={name}
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  required
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </div>
+
+              <button
+                type="button"
+                onClick={handleAdd}
+                className="mb-2 me-2 rounded-full border border-gray-200 bg-white p-4 px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              >
+                {" "}
+                ➕ Add
+              </button>
+              <button
+                type="button"
+                onClick={handleDelete}
+                className="me-2rounded-full mb-2 border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              >
+                ❌ Delete
+              </button>
+            </div>
+          </form>
+        </div>
+        {/* <h5 className="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Preparation
       </h5>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
@@ -124,6 +216,7 @@ function IngredientList(props: IngredientListProps) {
           />
         </svg>
       </a> */}
+      </div>
     </div>
   );
 }
