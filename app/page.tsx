e8094a9e480a8aa55d94bb8c61306f0eca82e0a7
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-28 17:55:34 -0400

    add save button to the title

--- app/page.tsx
+++ app/page.tsx
@@ -262,18 +262,21 @@ function Title(props: Titleprops) {
           onClick={onSave}
           className="mb-2 me-2 rounded-full border border-gray-200 bg-white p-4 px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
         >
-          ✅ Save
+          ➕ Add
         </button>
       )}
 
       {!editMode && (
-        <button
-          type="button"
-          onClick={onEdit}
-          className="mb-2 me-2 rounded-full border border-gray-200 bg-white p-4 px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-        >
-          ✏️ Edit
-        </button>
+        <>
+          <button
+            type="button"
+            onClick={onEdit}
+            className="mb-2 me-2 rounded-full border border-gray-200 bg-white p-4 px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+          >
+            ✏️ Edit
+          </button>
+          <Button> ✅ Save</Button>
+        </>
       )}
     </div>
   );
